824078aa859c0d7ef9747ae4e861abd4dbf5818d
XeO3-git <lgsky8@gmail.com>
2026-07-28 20:19:45 -0400

    made lines follow correct path. Still have to fix lines layering though

--- Graph.py
+++ Graph.py
@@ -3,7 +3,7 @@ import Grid
 import csv
 
 data_file = "data.csv"
-size = 2;
+size = 50;
 
 # take data from csv
 with open(data_file, 'r') as csvfile:
@@ -22,12 +22,19 @@ for row in list_of_csv:
 fig, ax = plt.subplots()
 
 for x in range(size): # create dot grid
+    ax.plot([x, x], [0, size], "black", linewidth=1)
+    ax.plot([0, size], [x,x], "black", linewidth=1)
     for y in range(size):
-        ax.plot(x, y, "ko")
+        ax.plot(x, y, "ko", markersize=(int(10*1/size))+3)
 
 # plot data
 for path in list_of_paths:
-    ax.plot([path.x1, path.x2], [path.y1, path.y2], path.get_color(), linewidth=4)
+
+    ax.plot(path.x1, path.y1, path.get_color(), marker='o', markersize=(int(15*1/size))+5)
+    ax.plot(path.x2, path.y2, path.get_color(), marker='o', markersize=(int(15*1/size))+5)
+
+    for line in path.get_lines():
+        ax.plot([line.x1, line.x2], [line.y1, line.y2], path.get_color(), linewidth=2)
 
 ax.set_aspect("equal")
 plt.show()
--- Grid.py
+++ Grid.py
@@ -1,3 +1,4 @@
+import math
 class PathData:
     def __init__(self, resistance: int, x1: int, y1: int, x2: int, y2: int):
         self.resistance = int(resistance)
@@ -15,3 +16,13 @@ class PathData:
         else: 
             return "red"
     # resistance on one line ~= 20 ohms
+    def get_lines(self):
+        line1 = Line(self.x1,self.y1,self.x2,self.y1)
+        line2 = Line(self.x2,self.y1,self.x2,self.y2)
+        return [line1, line2]
+class Line:
+    def __init__(self, x1: int, y1: int, x2: int, y2: int):
+        self.x1 = int(x1)
+        self.x2 = int(x2)
+        self.y1 = int(y1)
+        self.y2 = int(y2)
